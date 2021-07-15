cd85fe9a2840bbf2f9a5a502445bce267fe25e9b
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2021-07-15 11:16:58 +0200

    Use rust logging facade and simplelog to log (#1)

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,23 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+use log::{info, LevelFilter};
+use simplelog::{ColorChoice, ConfigBuilder, TermLogger, TerminalMode};
+
 fn main() {
-    println!("Hello, world!");
+    TermLogger::init(
+        LevelFilter::Debug,
+        ConfigBuilder::new()
+            .set_thread_level(LevelFilter::Error)
+            .set_target_level(LevelFilter::Error)
+            .set_location_level(LevelFilter::Error)
+            .set_time_format_str("%Y-%m-%d %H:%M:%S %z")
+            .set_time_to_local(true)
+            .build(),
+        TerminalMode::Mixed,
+        ColorChoice::Auto,
+    )
+    .unwrap();
+
+    info!("Hello, world!");
 }
