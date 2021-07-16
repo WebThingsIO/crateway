45f011e2ea87b240fb2f3eb2741071c1b8f9ef22
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2021-07-16 18:09:46 +0200

    Start addon

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,18 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+mod addon_instance;
+mod addon_socket;
+mod process_manager;
+
+use crate::process_manager::{ProcessManager, StartAddon};
+use actix::prelude::*;
+use dirs::home_dir;
 use log::{info, LevelFilter};
 use simplelog::{ColorChoice, ConfigBuilder, TermLogger, TerminalMode};
 
-fn main() {
+#[actix_web::main]
+async fn main() {
     TermLogger::init(
         LevelFilter::Debug,
         ConfigBuilder::new()
@@ -20,5 +28,22 @@ fn main() {
     )
     .unwrap();
 
-    info!("Hello, world!");
+    info!("Starting gateway");
+
+    let mut home_dir = home_dir().expect("Get home dir");
+
+    home_dir.push(".webthings2");
+
+    let mut addon_dir = home_dir.clone();
+    addon_dir.push("addons");
+    addon_dir.push("test-adapter");
+
+    ProcessManager::from_registry().do_send(StartAddon {
+        home: home_dir,
+        id: String::from("test-adapter"),
+        path: addon_dir,
+        exec: String::from("{path}/target/debug/{name}"),
+    });
+
+    addon_socket::start().await.expect("Starting addon socket");
 }
