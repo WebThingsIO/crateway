cf0888b386cf79108b0327779cabe77329a30f9e
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2021-08-04 23:40:35 +0200

    Add device handling (#6)

--- src/addon_instance.rs
+++ src/addon_instance.rs
@@ -3,17 +3,21 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+use crate::adapter::Adapter;
 use crate::addon_manager::{AddonManager, AddonStarted};
 use actix::prelude::*;
 use actix::{Actor, StreamHandler};
 use actix_web_actors::ws;
 use log::{debug, error, trace};
+use std::collections::HashMap;
 use std::error::Error;
 use webthings_gateway_ipc_types::{
     Message, MessageBase, PluginRegisterResponseMessageData, Preferences, Units, UserProfile,
 };
 
-pub struct AddonInstance;
+pub struct AddonInstance {
+    adapters: HashMap<String, Adapter>,
+}
 
 impl Actor for AddonInstance {
     type Context = ws::WebsocketContext<Self>;
@@ -46,48 +50,67 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for AddonInstance {
 
 impl AddonInstance {
     pub fn new() -> Self {
-        Self
+        Self {
+            adapters: HashMap::new(),
+        }
     }
 
     pub fn on_msg(
-        &self,
+        &mut self,
         msg: Message,
         ctx: &mut ws::WebsocketContext<Self>,
     ) -> Result<(), Box<dyn Error>> {
         debug!("Received {:?}", msg);
 
-        if let Message::PluginRegisterRequest(msg) = msg {
-            let id = msg.plugin_id();
+        match msg {
+            Message::PluginRegisterRequest(msg) => {
+                let id = msg.plugin_id();
 
-            AddonManager::from_registry().do_send(AddonStarted {
-                id: id.to_owned(),
-                addr: ctx.address(),
-            });
+                AddonManager::from_registry().do_send(AddonStarted {
+                    id: id.to_owned(),
+                    addr: ctx.address(),
+                });
 
-            let response: Message = PluginRegisterResponseMessageData {
-                gateway_version: env!("CARGO_PKG_VERSION").to_owned(),
-                plugin_id: id.to_owned(),
-                preferences: Preferences {
-                    language: "en-US".to_owned(),
-                    units: Units {
-                        temperature: "degree celsius".to_owned(),
+                let response: Message = PluginRegisterResponseMessageData {
+                    gateway_version: env!("CARGO_PKG_VERSION").to_owned(),
+                    plugin_id: id.to_owned(),
+                    preferences: Preferences {
+                        language: "en-US".to_owned(),
+                        units: Units {
+                            temperature: "degree celsius".to_owned(),
+                        },
                     },
-                },
-                user_profile: UserProfile {
-                    addons_dir: "".to_owned(),
-                    base_dir: "".to_owned(),
-                    config_dir: "".to_owned(),
-                    data_dir: "".to_owned(),
-                    gateway_dir: "".to_owned(),
-                    log_dir: "".to_owned(),
-                    media_dir: "".to_owned(),
-                },
-            }
-            .into();
+                    user_profile: UserProfile {
+                        addons_dir: "".to_owned(),
+                        base_dir: "".to_owned(),
+                        config_dir: "".to_owned(),
+                        data_dir: "".to_owned(),
+                        gateway_dir: "".to_owned(),
+                        log_dir: "".to_owned(),
+                        media_dir: "".to_owned(),
+                    },
+                }
+                .into();
 
-            debug!("Sending {:?}", &response);
-            ctx.text(serde_json::to_string(&response)?);
-        }
+                debug!("Sending {:?}", &response);
+                ctx.text(serde_json::to_string(&response)?);
+            }
+            Message::AdapterAddedNotification(msg) => {
+                let adapter = Adapter::new(msg.data.adapter_id.clone());
+                self.adapters.insert(msg.data.adapter_id, adapter);
+            }
+            Message::DeviceAddedNotification(msg) => {
+                match self.adapters.get_mut(&msg.data.adapter_id) {
+                    Some(adapter) => {
+                        adapter.add_device(msg.data.device);
+                    }
+                    None => {
+                        error!("No adapter with id {} found", msg.data.adapter_id)
+                    }
+                }
+            }
+            _ => {}
+        };
 
         Ok(())
     }
