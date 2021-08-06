b9be83e30d9892e8088c1ee84d4f34e4340266f0
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2021-08-06 17:53:03 +0200

    Fix build errors (#16)

--- src/adapter.rs
+++ src/adapter.rs
@@ -3,6 +3,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 use crate::device::Device;
+use anyhow::{anyhow, Error};
 use std::collections::HashMap;
 use webthings_gateway_ipc_types::{Device as DeviceDescription, Property as PropertyDescription};
 
@@ -38,16 +39,14 @@ impl Adapter {
         &mut self,
         device_id: String,
         property: PropertyDescription,
-    ) -> Result<(), String> {
-        match self.devices.get_mut(&device_id) {
-            Some(device) => {
-                device.update_property(property)?;
-                Ok(())
-            }
-            None => Err(format!(
-                "Device {} does not exist in adapter {}",
-                device_id, self.id
-            )),
-        }
+    ) -> Result<(), Error> {
+        let id = self.id.clone();
+
+        let device = self
+            .devices
+            .get_mut(&device_id)
+            .ok_or_else(|| anyhow!("Device {} does not exist in adapter {}", device_id, id))?;
+
+        device.update_property(property)
     }
 }
--- src/device.rs
+++ src/device.rs
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+use anyhow::{anyhow, Error};
 use log::debug;
 use webthings_gateway_ipc_types::{Device as DeviceDescription, Property};
 
@@ -10,24 +11,25 @@ pub struct Device {
 }
 
 impl Device {
-    pub(crate) fn update_property(&mut self, new_property: Property) -> Result<(), String> {
+    pub(crate) fn update_property(&mut self, new_property: Property) -> Result<(), Error> {
+        let id = self.description.id.clone();
+
         let name = new_property
             .name
             .clone()
-            .ok_or_else(|| String::from("Property has no name"))?;
+            .ok_or_else(|| anyhow!("Property has no name"))?;
         let properties = self
             .description
             .properties
             .as_mut()
-            .ok_or(format!("Device {} has no properties", self.description.id))?;
-        let property = properties.get_mut(&name).ok_or(format!(
-            "Device {} has no property called {}",
-            self.description.id, name
-        ))?;
+            .ok_or_else(|| anyhow!("Device {} has no properties", id))?;
+        let property = properties
+            .get_mut(&name)
+            .ok_or_else(|| anyhow!("Device {} has no property called {}", id, name))?;
         if property.value != new_property.value {
             debug!(
                 "Property {} of device {} changed from {:?} to {:?}",
-                name, self.description.id, property.value, new_property.value
+                name, id, property.value, new_property.value
             );
         }
         properties.insert(name, new_property);
